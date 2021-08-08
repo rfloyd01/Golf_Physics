eac62f47c3b4c115b5338b5104053a252a4252e0
Robert Floyd <70585609+rfloyd01@users.noreply.github.com>
2021-08-08 14:34:45 -0400

    Update Physics.h

--- Headers/Physics.h
+++ Headers/Physics.h
@@ -5,3 +5,4 @@
 #include "eigen.h"
 
 std::pair<Vector_3d, Vector_3d> getBallVelocities(Golf_Club &club, Golf_Ball &ball, Golf_Swing &swing);
+void calculateBallFlight(std::pair<Vector_3d, Vector_3d> bi, Golf_Ball& ball, double delta_t = 0.0001);
